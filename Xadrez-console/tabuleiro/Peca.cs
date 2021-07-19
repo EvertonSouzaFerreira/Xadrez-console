a2449e092e63450df89ef709d993c93e3ce13471
Everton Ferreira <everton.souzaferreira@outlook.com>
2021-07-19 21:14:28 +0200

    Criando a clase tabuleiro

---
+++ Xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xadrez_console.tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; set; }
+        public int qteMovimentos { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        {
+            this.posicao = posicao;
+            this.cor = cor;
+            this.tab = tab;
+            this.qteMovimentos = 0;
+        }
+    }
+}
